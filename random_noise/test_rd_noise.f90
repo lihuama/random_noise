3ab481beb8082b82dfa2da6296b00282dda2b597
流年 <964652891@qq.com>
2018-08-21 15:12:34 +0800

    write

--- random_noise/test_rd_noise.f90
+++ random_noise/test_rd_noise.f90
@@ -1,13 +1,24 @@
 PROGRAM test_rd_noise
-   REAL, DIMENSION(100) :: y,y0
-   REAL :: r
-   INTEGER :: i
+    INTEGER, PARAMETER :: MAX_SIZE = 100
+    REAL, DIMENSION(MAX_SIZE) :: y,y0
+    REAL :: r
+    INTEGER :: i,istat
+    CHARACTER(len=80) :: msg
    
-   y = [(1.2+2.*i+3.2*i**2, i=1,100)]
-   y0 = y
-   CALL rd_noise(y,100,2.)
-   WRITE (*,'(3X,A,6X,A2,6X,A4)') 'y0','y','y-y0'
-   WRITE (*,102) (y0(i),y(i),y(i)-y0(i),i=1,10)
-102 FORMAT(/,3F8.3)
+    y = [(1.2+2.*i+3.2*i**2, i=1,MAX_SIZE)]
+    y0 = y
+    CALL rd_noise(y,MAX_SIZE,2.)
+   
+    OPEN(UNIT=10,FILE='noise.dat',STATUS='REPLACE',ACTION='WRITE',IOSTAT=istat,iomsg=msg)
+    IF( ISTAT==0 ) THEN
+    WRITE (10,101) (i,y(i),i=1,MAX_SIZE)
+101 FORMAT(1X,I0,T6,F12.6) 
+    ELSE
+        WRITE (*,*) TRIM(msg)
+    END IF
+   
+    WRITE (*,'(3X,A,6X,A2,6X,A4)') 'y0','y','y-y0'
+    WRITE (*,102) (y0(i),y(i),y(i)-y0(i),i=1,10)
+102 FORMAT(/,3F8.3)    
     
 END PROGRAM
